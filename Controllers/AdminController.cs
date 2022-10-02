0878b446fe8a41306ee1a6d8669ee0109e54716f
Omar <annexomar@gmail.com>
2022-10-02 20:25:30 +0300

    Create Role

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,26 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using CompanyMvc.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace CompanyMvc.Controllers
 {
     public class AdminController : Controller
     {
-        private readonly ILogger<AdminController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(ILogger<AdminController> logger)
+        public AdminController(RoleManager<IdentityRole> roleManager)
         {
-            _logger = logger;
+            _roleManager = roleManager;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRole)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole()
+                {
+                    Name = createRole.RoleName
+                };
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+
 
     }
 }
---
+++ Models/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyMvc.Models.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        public string RoleName { get; set; } = null!;
+    }
+}
